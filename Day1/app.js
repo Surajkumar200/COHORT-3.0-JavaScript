31472a15eb4a8fba95833efd1c84da0d09d33f76
Suraj Kumar Shaw <surajshaw7778@gmail.com>
2026-06-18 23:22:39 +0530

    day1-phase1

---
+++ Day1/app.js
@@ -0,0 +1,84 @@
+// ***** console methods(warn, error, table)
+// console.log(1233)
+// console.log(`hekki ` + " agdu")
+// console.warn('this is warning');
+// console.info("ds")
+/*console.error("this is error");
+console.table(["suraj" ,"puja","sonu"])
+*/
+
+
+//*****  Variables — var, let, const
+// var a //declaration
+// a = 12 //initialization
+// var a = 12 // declaration + initilization
+// a = 20 // uptation or reInitilization
+
+
+// let a, b;
+// a = 20;
+// b = 30
+// console.log("value of a is" ,a , "value of b is", b)
+
+
+// ***** Operators
+// *****Arithmetic
+// a + b
+// a - b
+// a * b
+// a / b
+// a % b  modulus
+// a ** b power
+
+// **** Data Types
+
+//  -- premitive
+//     - Number (10, 20.55, -45, 333333333)
+//     - String ('h', ,"suraj", `hello`)
+//     - Boolean (true, false)
+//     - Undefined
+//     - Null
+//     - BigInt(number;s rang is 2^53-1 and beyond that)
+//     - Symbol (unique value)
+
+// var s1 = Symbol("hi")
+// var s2 = Symbol("hi")
+// s1 == s2  false
+
+// ****typeof
+
+// typeof(null) = object
+
+
+// ***** alert confirm prompt
+
+// alert("hello viewers")
+
+// var value = confirm("are tou 18 plus+")
+// console.log(value)
+// function gr() {
+//     if (value) {
+//         console.log("by")
+//     }
+//     else {
+//         console.log("hello ")
+//     }
+// }
+// gr()
+
+
+// let user = prompt("enter your name")
+// console.log("hello", user)
+// console.log(456464)
+
+// var  num1 = Number( prompt("Enter your num1"))
+// var num2 = Number(prompt("Enter your num2"))
+
+// console.log(num1+num2);
+
+
+
+
+
+// ****in console number is alwyas in color text
+// ****in console string is alwyas in white-color text
