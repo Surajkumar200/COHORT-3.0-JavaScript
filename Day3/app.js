4f9046b1648fb4b2092d02c31000d375e6d5ba29
Suraj Kumar Shaw <surajshaw7778@gmail.com>
2026-06-20 12:33:02 +0530

    day3-phase1

---
+++ Day3/app.js
@@ -0,0 +1,72 @@
+// **** loops
+//  *** while loop
+// let list = document.querySelector("div")
+
+
+    // var num = Number(prompt("enter number"));
+    // var a = 0;
+    // while (a < num) {
+    //     console.log(a)
+    
+    
+    //     a++
+    // } 
+  
+
+    
+// var num = Number(prompt("Enter a number"))
+// var a = 1
+// while (a < 10) {
+//     console.log(num + "*" + a + "="+ num * a)
+//     a++
+// }
+
+// *****do while 
+// do {
+//     console.log(num + " *" + a + "=" + num * a)
+//     a++
+// } while (a < 10)
+
+
+//  ***For 
+// for (let a = 1; a <= 10; a++) {
+//     console.log("fredam"+a)
+// }
+
+// for (let a = 10; a > 0; a--){
+//     console.log(a)
+// }
+
+// let s = "Hello, World!";
+
+// console.log(s.length);              // 13
+// console.log(s.toUpperCase());       // "HELLO, WORLD!"
+// console.log(s.toLowerCase());       // "hello, world!"
+// console.log(s.indexOf("World"));    // 7  (position of "World")
+// console.log(s.includes("Hello"));   // true
+// console.log(s.slice(0, 5));         // "Hello"
+// console.log(s.substring(7, 12));    // "World"
+// console.log(s.replace("World", "JS"));   // "Hello, JS!"
+// console.log(s.split(", "));         // ["Hello", "World!"]
+// console.log("   hi   ".trim());     // "hi"
+// console.log("abc".repeat(3));       // "abcabcabc"
+// console.log(s.startsWith("Hello")); // true
+// console.log(s.endsWith("!"));       // true
+// console.log(s.charAt(0));           // "H"
+// console.log(s[0]);                  // "H" (also works)
+
+// *** break & continue
+
+// for (let i = 1; i <= 10; i++) {
+//     if (i === 5) break;
+//     console.log(i);
+// }
+// Prints: 1, 2, 3, 4
+
+// for (let i = 1; i <= 5; i++) {
+//     if (i === 3) continue;
+//     console.log(i);
+// }
+// Prints: 1, 2, 4, 5  (3 is skipped)  after continue the below code skipped
+
+
